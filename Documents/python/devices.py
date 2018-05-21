ea68239c5c4f8b7e6955a74f77b347defedc8431
shubham-khulbe <skhulbe@dal.ca>
2018-05-21 17:41:02 +0000

    secret argument added

--- Documents/python/devices.py
+++ Documents/python/devices.py
@@ -5,6 +5,7 @@ cisco_ios1 = {
 		'ip' : '192.168.122.99',
 		'username' : 'shubham',
 		'password' : 'shubham',
+		'secret' : 'shubham',
 		'port' : 22,
 	}
 
@@ -13,7 +14,8 @@ cisco_ios2 = {
                 'ip' : '192.168.122.98',
                 'username' : 'shubham',
                 'password' : 'shubham',
-                'port' : 22,
+                'secret' : 'shubham',
+		'port' : 22,
         }
 
 cisco_ios3 = {
@@ -21,13 +23,16 @@ cisco_ios3 = {
                 'ip' : '192.168.122.15',
                 'username' : 'shubham',
                 'password' : 'shubham',
-                'port' : 22,
+                'secret' : 'shubham',
+		'port' : 22,
         }
-
+"""
 cisco_ios4 = {
                 'device_type' : 'cisco_ios_ssh',
                 'ip' : '192.168.122.55',
                 'username' : 'shubham',
                 'password' : 'shubham',
-                'port' : 22,
+                'secret' : 'shubham',
+		'port' : 22,
         }
+"""
