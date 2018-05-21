8d480174d5cf84d54a1188f198b848007688bdd0
shubham-khulbe <skhulbe@dal.ca>
2018-05-21 16:16:00 +0000

    device details

---
+++ Documents/python/devices.py
@@ -0,0 +1,33 @@
+""" Information of network devices  """
+
+cisco_ios1 = {
+		'device_type' : 'cisco_ios_ssh',
+		'ip' : '192.168.122.99',
+		'username' : 'shubham',
+		'password' : 'shubham',
+		'port' : 22,
+	}
+
+cisco_ios2 = {
+                'device_type' : 'cisco_ios_ssh',
+                'ip' : '192.168.122.98',
+                'username' : 'shubham',
+                'password' : 'shubham',
+                'port' : 22,
+        }
+
+cisco_ios3 = {
+                'device_type' : 'cisco_ios_ssh',
+                'ip' : '192.168.122.15',
+                'username' : 'shubham',
+                'password' : 'shubham',
+                'port' : 22,
+        }
+
+cisco_ios4 = {
+                'device_type' : 'cisco_ios_ssh',
+                'ip' : '192.168.122.55',
+                'username' : 'shubham',
+                'password' : 'shubham',
+                'port' : 22,
+        }
