2d517a78fca54d3bcb8a16cd5ed3ca4488c8efa2
shubham-khulbe <skhulbe@dal.ca>
2018-05-24 05:10:47 +0000

    test file updated

--- Documents/python/get_details_test.py
+++ Documents/python/get_details_test.py
@@ -16,7 +16,7 @@ r3 = driver(hostname=host_ip,username=user_name,password=pass_word,optional_args
 r3.open()
 
 """
-def menu():
+def ind_details():
 	host_ip = input("Please enter your host device's IP : ")
 	user_name = input ("Please enter your host device's USERNAME : ")
 	pass_word = input("Please enter your PASSWORD : ")
@@ -24,7 +24,7 @@ def menu():
 
 	driver = get_network_driver('ios')
 	optional_argument = {'secret' : secret_pass}
-	router = driver(hostname=host_ip,username=user_name,password=pass_word,optional_args = otional_argument)
+	router = driver(hostname=host_ip,username=user_name,password=pass_word,optional_args = optional_argument)
 
 	router.open()
 	ans=True
@@ -39,22 +39,24 @@ def menu():
     		""")
 		ans=input("What would you like to do? ")
 		if ans=="1":
-			info.general_facts()
+			print("enter general facts")
+			info.general_facts(router,host_ip)
 		elif ans=="2":
-			info.interface_info()
+			info.interface_info(router,host_ip)
 		elif ans=="3":
-			info.mac_address_table()
+			info.mac_address_table(router,host_ip)
 		elif ans=="4":
-			info.arp_table()
+			info.arp_table(router,host_ip)
 		elif ans =="5":
-			info.display_neighbors()
+			info.display_neighbors(router,host_ip)
 		elif ans=="6":
 			main_menu()
 		else:
 			print("Please enter a valid option")
+			ans = True
 
 def main_menu():
-	choice = ''
+	choice = True
 	while choice:
 		print("""
 			1. GET INFORMTION FOR ALL DEVICES
@@ -65,42 +67,44 @@ def main_menu():
 		if choice == "1":
 			pass
 		elif choice == "2":
-			main()
+			ind_details()
 		elif choice == "3":
 			print("Good Bye")
 			exit()
 		else:
 			print("Enter a valid option")
+			choice = True
 
 class Get_facts():
 	def __init__(self):
 		pass
 
-	def general_facts():
+	def general_facts(self,router,host_ip):
+		print("inside general facts")
 		ios_output = router.get_facts()
 		print ('\n'*50)
 		print ('#'*15,' GENERAL INFORMATION FOR {}'.format(host_ip),'#'*15,'\n')
 		print(json.dumps(ios_output, indent =5 ))
 
-	def interface_info():
+	def interface_info(self,router,host_ip):
 		ios_output = router.get_interfaces()
 		print ('\n'*50)
 		print ('#'*15,' INTERFACES FOR {}'.format(host_ip),'#'*15,'\n')
 		print(json.dumps(ios_output, indent = 5))
 
-	def mac_address_table():
+	def mac_address_table(self,router,host_ip):
 		ios_output = router.get_mac_address_table()
 		print ('\n'*50)
 		print ('#'*15,' MAC ADDRESS TABLE FOR {}'.format(host_ip),'#'*15,'\n')
 		print(json.dumps(ios_output, indent = 5))
 
-	def arp_table():
+	def arp_table(self,router,host_ip):
 		ios_output = router.get_arp_table()
 		print ('\n'*50)
 		print ('#'*15,' ARP TABLE FOR {}'.format(host_ip),'#'*15,'\n')
 		print(json.dumps(ios_output, indent = 5))
 
-	def display_neighbors():
+	def display_neighbors(self,router,host_ip):
 		ios_output = router.get_lldp_neighbors()
 		print ('\n'*50)
 		print ('#'*15,' NEIGHBORS FOR {}'.format(host_ip),'#'*15,'\n')
